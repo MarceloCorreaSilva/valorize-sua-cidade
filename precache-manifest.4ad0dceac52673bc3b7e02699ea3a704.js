17c1ab08c9e360ec9071e7332e260b2963eec4cd
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2020-09-01 18:34:03 -0300

    Updates

---
+++ precache-manifest.4ad0dceac52673bc3b7e02699ea3a704.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33d841fffa9b7c7ef8a3ba1526159a9",
+    "url": "/valorize-sua-cidade/index.html"
+  },
+  {
+    "revision": "8d509d5238ef1ada9a93",
+    "url": "/valorize-sua-cidade/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "557bc7fc8cdf8ad74883",
+    "url": "/valorize-sua-cidade/static/js/2.ba98abb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/valorize-sua-cidade/static/js/2.ba98abb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d509d5238ef1ada9a93",
+    "url": "/valorize-sua-cidade/static/js/main.011fe16e.chunk.js"
+  },
+  {
+    "revision": "97befd6c662546ed08a9",
+    "url": "/valorize-sua-cidade/static/js/runtime-main.e349ad37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/valorize-sua-cidade/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3ebf15a5768b42153ea43414bc3a0260.js"
+  "/valorize-sua-cidade/precache-manifest.4ad0dceac52673bc3b7e02699ea3a704.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/valorize-sua-cidade/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
