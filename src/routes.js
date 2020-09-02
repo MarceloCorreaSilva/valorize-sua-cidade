68a4fb14933e20c974750fc083743563d9f2f254
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2020-09-02 18:12:29 -0300

    defining route file

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+import Home from "./pages/Home";
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/valorize-sua-cidade" component={Home} exact />
+        <Route>
+          <Redirect component={Home} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
