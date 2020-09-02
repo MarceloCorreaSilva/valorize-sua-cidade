6a42e9134161235dc7bb1f91cd25be3dd269d60a
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2020-09-02 18:12:10 -0300

    defining new components

---
+++ src/components/Map/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import GoogleMap from 'google-map-react';
+
+function Map() {
+
+  return (
+    <GoogleMap></GoogleMap>
+  );
+}
+
+export default Map;
---
+++ src/components/Table/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Table() {
+
+  return (
+    <div className="lista uk-margin"></div>
+  );
+}
+
+export default Table;
