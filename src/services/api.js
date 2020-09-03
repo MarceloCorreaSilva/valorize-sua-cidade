3e7386752dfc93aed2b71a010a840f2ce61b62f1
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2020-09-03 20:46:53 -0300

    google spreadsheet connection

---
+++ src/services/api.js
@@ -0,0 +1,66 @@
+import { GoogleSpreadsheet } from "google-spreadsheet";
+
+// Credentials
+import credentials from "../credentials/google-sheets-api.json";
+
+const getAllProductors = async () => {
+  const doc = new GoogleSpreadsheet(process.env.REACT_APP_GOOGLE_SPREADSHEET);
+
+  await doc.useServiceAccountAuth({
+    client_email: credentials.client_email,
+    private_key: credentials.private_key,
+    // client_email: process.env.REACT_APP_GOOGLE_CLIENT_EMAIL,
+    // private_key: process.env.REACT_APP_GOOGLE_PRIVATE_KEY,
+  });
+
+  await doc.loadInfo(); // loads document properties and worksheets
+  //   console.log(doc.title);
+
+  const sheet = doc.sheetsByIndex[0];
+  const rows = await sheet.getRows();
+  const itens = rows.map(
+    ({
+      id,
+      data_atualizacao,
+      tipo,
+      nome,
+      proprietario,
+      inscricao_estadual,
+      empregados,
+      area_total,
+      area_de_horta,
+      area_de_pomar,
+      irrigacao,
+      cultivo_protegido,
+      veiculos,
+      comercializacao,
+      latitude,
+      longitude,
+    }) => {
+      return {
+        id,
+        data_atualizacao,
+        tipo,
+        nome,
+        proprietario,
+        inscricao_estadual,
+        empregados,
+        area_total,
+        area_de_horta,
+        area_de_pomar,
+        irrigacao,
+        cultivo_protegido,
+        veiculos,
+        comercializacao,
+        latitude,
+        longitude,
+      };
+    }
+  );
+  //   console.log(sheet.title);
+  //   console.log(itens);
+
+  return itens;
+};
+
+export default { getAllProductors };
