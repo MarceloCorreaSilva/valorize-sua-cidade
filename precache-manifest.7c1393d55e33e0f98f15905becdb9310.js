e0d3c7929673d17ba6f104147fa4dbe2c3cff530
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2020-09-04 11:47:36 -0300

    Updates

---
+++ precache-manifest.7c1393d55e33e0f98f15905becdb9310.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "688c547907415e021ae04a6e36b18f89",
+    "url": "/valorize-sua-cidade/index.html"
+  },
+  {
+    "revision": "ae30125d9142cfe3b5c3",
+    "url": "/valorize-sua-cidade/static/js/2.98664ba8.chunk.js"
+  },
+  {
+    "revision": "9d9c3c03e847d3018c7dc000e54d7d87",
+    "url": "/valorize-sua-cidade/static/js/2.98664ba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "646410a9c45b2b9ffb45",
+    "url": "/valorize-sua-cidade/static/js/main.4d44bad7.chunk.js"
+  },
+  {
+    "revision": "97befd6c662546ed08a9",
+    "url": "/valorize-sua-cidade/static/js/runtime-main.e349ad37.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/valorize-sua-cidade/precache-manifest.fc03b6fdfc08d5ed830bfece1b2a641a.js"
+  "/valorize-sua-cidade/precache-manifest.7c1393d55e33e0f98f15905becdb9310.js"
 );
 
 self.addEventListener('message', (event) => {
