97eeb3c0442a69304896157bd55bff750f8288a1
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2020-09-04 00:15:26 -0300

    convert project to typescript and fix errors

--- src/components/Html/Select/index.tsx
+++ src/components/Html/Select/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactSelect, { Props } from "react-select";
+import ReactSelect, { Props, Theme } from "react-select";
 
 const Select: React.FC<Props> = ({
   name,
@@ -9,16 +9,16 @@ const Select: React.FC<Props> = ({
   options,
   onChange,
 }) => {
-  // function customTheme(theme) {
-  //   return {
-  //     ...theme,
-  //     colors: {
-  //       ...theme.colors,
-  //       primary25: "#D3D3D3",
-  //     },
-  //     borderRadius: false,
-  //   };
-  // }
+  function customTheme(theme: Theme): Theme {
+    return {
+      ...theme,
+      colors: {
+        ...theme.colors,
+        primary25: "#D3D3D3",
+      },
+      borderRadius: 0,
+    };
+  }
 
   return (
     <div className="uk-form-controls">
@@ -30,7 +30,7 @@ const Select: React.FC<Props> = ({
         value={value}
         options={options}
         onChange={onChange}
-        // theme={customTheme}
+        theme={customTheme}
         // className="basic-multi-select"
         // classNamePrefix="select"
       />
