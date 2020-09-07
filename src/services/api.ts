78922487d0a7fcbf4de96c252bc69a32678ba82f
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2020-09-07 10:32:19 -0300

    update 2020-09-07

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 const URL_BACKEND = window.location.hostname.includes('localhost')
   ? 'http://localhost:8000'
-  : 'https://valorize-sua-cidade.herokuapp.com/';
+  : 'https://valorize-sua-cidade.herokuapp.com';
 
 export default {
   URL_BACKEND,
--- src/services/google.spreadsheets.ts
+++ src/services/google.spreadsheets.ts
@@ -1,15 +1,17 @@
 import { GoogleSpreadsheet } from "google-spreadsheet";
 
 // Credentials
-import credentials from "../credentials/google-sheets-api.json";
+// import credentials from "../credentials/google-sheets-api.json";
 
 const doc = new GoogleSpreadsheet(
   process.env.REACT_APP_GOOGLE_SPREADSHEET as string
 );
 
 doc.useServiceAccountAuth({
-  client_email: credentials.client_email,
-  private_key: credentials.private_key
+  // client_email: credentials.client_email,
+  // private_key: credentials.private_key,
+  client_email: process.env.REACT_APP_GOOGLE_CLIENT_EMAIL as string,
+  private_key: process.env.REACT_APP_GOOGLE_PRIVATE_KEY as string
 });
 
 export { doc };
