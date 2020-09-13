e45f6767c3f3001c4bbe7206f6b1f42289c9074b
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2020-09-13 10:02:20 -0300

    creating google maps component

---
+++ src/components/GoogleMaps/index.tsx
@@ -0,0 +1,82 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect } from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+
+// Components || Interfaces
+import { Producer } from "../../repositories/Producer";
+import { Highlighter } from "../../repositories/Highlighter";
+
+interface Props {
+  producers: Producer[];
+  highlighters: Highlighter[];
+}
+
+const Map: React.FC<Props> = ({ producers, highlighters }) => {
+  const onLoad = (data: any) => {
+    // console.log("data: ", data);
+  };
+
+  useEffect(() => {
+    // console.log("highlighters: ", highlighters);
+  }, [highlighters]);
+
+  return (
+    <LoadScript googleMapsApiKey="AIzaSyBXfldFNZ4WTjwn56GltKgQRL2XOam66yU">
+      <GoogleMap
+        mapContainerStyle={{
+          width: "100%",
+          height: "600px",
+        }}
+        center={{
+          lat: -18.675444,
+          lng: -53.64186,
+        }}
+        zoom={10}
+      >
+        {highlighters &&
+          highlighters.map((highlighter: Highlighter, key) => {
+            return (
+              <Marker
+                key={key}
+                title={highlighter.name}
+                onLoad={onLoad}
+                position={{
+                  lat: highlighter.lat,
+                  lng: highlighter.lng,
+                }}
+              >
+                {/* <InfoWindow
+                  onLoad={onLoad}
+                  position={{
+                    lat: highlighter.lat,
+                    lng: highlighter.lng,
+                  }}
+                >
+                  <div
+                    style={{
+                      background: `white`,
+                      border: `1px solid #ccc`,
+                      padding: 15,
+                    }}
+                  >
+                    <h1>{highlighter.name}</h1>
+                  </div>
+                </InfoWindow> */}
+              </Marker>
+            );
+          })}
+
+        {producers.map((producer: Producer, key) => (
+          <Marker
+            key={key}
+            title={producer.nome}
+            onLoad={onLoad}
+            position={{ lat: producer.latitude, lng: producer.longitude }}
+          />
+        ))}
+      </GoogleMap>
+    </LoadScript>
+  );
+};
+
+export default React.memo(Map);
