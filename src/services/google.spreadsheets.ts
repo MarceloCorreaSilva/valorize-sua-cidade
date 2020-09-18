feefdb24ef92a40e083c9793574bb5192de7e151
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2020-09-18 10:53:47 -0300

    use credentials as environment variables

--- src/services/google.spreadsheets.ts
+++ src/services/google.spreadsheets.ts
@@ -1,17 +1,22 @@
 import { GoogleSpreadsheet } from "google-spreadsheet";
 
 // Credentials
-import credentials from "../_credentials/google-sheets-api.json";
+// import credentials from "../_credentials/google-sheets-api.json";
+
+const credentials = {
+  client_email: String(process.env.REACT_APP_GOOGLE_CLIENT_EMAIL),
+  private_key: String(process.env.REACT_APP_GOOGLE_PRIVATE_KEY)
+    .replaceAll("\\n", "\\\n")
+    .replaceAll("\\", ""),
+};
 
 const doc = new GoogleSpreadsheet(
-  process.env.REACT_APP_GOOGLE_SPREADSHEET as string
+  String(process.env.REACT_APP_GOOGLE_SPREADSHEET)
 );
 
 doc.useServiceAccountAuth({
   client_email: credentials.client_email,
   private_key: credentials.private_key,
-  // client_email: process.env.REACT_APP_GOOGLE_CLIENT_EMAIL as string,
-  // private_key: process.env.REACT_APP_GOOGLE_PRIVATE_KEY as string
 });
 
 export { doc };
