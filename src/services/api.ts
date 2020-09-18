bf025673203c3d99e884e31f7e934d63a72c1616
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2020-09-18 10:59:19 -0300

    putting files in .gitignore

--- src/services/api.ts
+++
@@ -1,7 +0,0 @@
-const URL_BACKEND = window.location.hostname.includes('localhost')
-  ? 'http://localhost:8000'
-  : 'https://valorize-sua-cidade.herokuapp.com';
-
-export default {
-  URL_BACKEND,
-};
