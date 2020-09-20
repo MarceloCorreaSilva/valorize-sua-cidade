c08fad0b7fa2a9ec004f081a8d08e499ced4be09
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2020-09-20 15:43:23 -0300

    configuration file

--- src/components/GoogleMaps/index.tsx
+++ src/components/GoogleMaps/index.tsx
@@ -2,6 +2,9 @@
 import React, { useEffect, useState } from "react";
 import { GoogleMap, LoadScript, Marker, Polygon } from "@react-google-maps/api";
 
+// Configs
+import config from "../../config";
+
 // Components || Interfaces
 import { Producer } from "../../repositories/Producer";
 import { Highlighter } from "../../repositories/Highlighter";
@@ -20,10 +23,7 @@ interface LatLng {
 }
 
 const Map: React.FC<Props> = ({ producers, highlighters }) => {
-  const [coordinates] = useState({
-    lat: Number(process.env.REACT_APP_CITY_LAT),
-    lng: Number(process.env.REACT_APP_CITY_LNG),
-  });
+  const [coordinates] = useState(config.city.coordinate);
   const [geoJSONOfTheMunicipality, setGeoJSONOfTheMunicipality] = useState({
     type: "Feature",
     properties: {
@@ -39,21 +39,22 @@ const Map: React.FC<Props> = ({ producers, highlighters }) => {
 
   useEffect(() => {
     const load = async () => {
-      const municipality = process.env.REACT_APP_CITY_MUNICIPALITY_CODE_AT_IBGE;
+      // const municipality = process.env.REACT_APP_CITY_MUNICIPALITY_CODE_AT_IBGE;
+      const municipality = config.city.code;
       const geojson2 = await import(
         `../../data/geodata/${municipality}.json`
       ).then((data) => {
         setGeoJSONOfTheMunicipality(data.features[0]);
-        console.dir(data.features[0]);
+        // console.dir(data.features[0]);
       });
 
       // setGeoJSONOfTheMunicipality(geojson2.default);
 
-      console.dir({
-        municipality,
-        // geojson,
-        geojson2,
-      });
+      // console.dir({
+      //   municipality,
+      //   // geojson,
+      //   geojson2,
+      // });
     };
 
     load();
@@ -94,7 +95,8 @@ const Map: React.FC<Props> = ({ producers, highlighters }) => {
         .replace(/\\f/g, "\\f");
       let coordinates = JSON.parse(geo);
       let coordArr = Array<LatLng>();
-      return console.log(geo);
+      // return console.log(geo);
+      return null;
     });
 
     let geo3 = geo2[0].coordinates
@@ -115,7 +117,7 @@ const Map: React.FC<Props> = ({ producers, highlighters }) => {
       coordArr.push({ lat: coordinate.lat, lng: coordinate.lng })
     );
 
-    console.log("DEBUG: ", coordArr);
+    // console.log("DEBUG: ", coordArr);
 
     return (
       <Polygon
@@ -170,27 +172,37 @@ const Map: React.FC<Props> = ({ producers, highlighters }) => {
 
         {highlighters &&
           highlighters.map((highlighter: Highlighter, key) => {
+            if (highlighter.lat !== null && highlighter !== null) {
+              return (
+                <Marker
+                  key={key}
+                  title={highlighter.name}
+                  onLoad={onLoad}
+                  position={{
+                    lat: highlighter.lat,
+                    lng: highlighter.lng,
+                  }}
+                />
+              );
+            }
+
+            return null;
+          })}
+
+        {producers.map((producer: Producer, key) => {
+          if (producer.latitude !== null && producer.longitude !== null) {
             return (
               <Marker
                 key={key}
-                title={highlighter.name}
+                title={producer.nome}
                 onLoad={onLoad}
-                position={{
-                  lat: highlighter.lat,
-                  lng: highlighter.lng,
-                }}
+                position={{ lat: producer.latitude, lng: producer.longitude }}
               />
-            );
-          })}
+            )
+          }
 
-        {producers.map((producer: Producer, key) => (
-          <Marker
-            key={key}
-            title={producer.nome}
-            onLoad={onLoad}
-            position={{ lat: producer.latitude, lng: producer.longitude }}
-          />
-        ))}
+          return null;
+        })}
       </GoogleMap>
     </LoadScript>
   );
